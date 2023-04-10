9c937906c41bf53babe39dd5b8a883d209361213
Elena Kochegarova <lelet@ngs.ru>
2023-04-10 12:34:43 +0700

    finished

---
+++ src/components/ShopItemFunc.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default class ShopItemFunc extends React.Component {
+  render() {
+    const { product } = this.props;
+
+    return (
+      <div class="main-content">
+        <h2>{product.brand}</h2>
+        <h1>{product.title}</h1>
+        <h3>{product.description}</h3>
+        <div class="description">{product.descriptionFull}</div>
+        <div class="highlight-window mobile"><div class="highlight-overlay"></div></div>
+        <div class="divider"></div>
+        <div class="purchase-info">
+          <div class="price">{product.currency}{product.price.toFixed(2)}</div>
+          <button>Добавить в корзину</button>
+        </div>
+      </div>
+    );
+  }
+}
